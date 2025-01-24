b9c11c3c7e0c343b1e7305d544f5a85af1721b02
ZahraThiaw <fatimatathiaw6@gmail.com>
2025-01-24 08:51:35 +0000

    first commit

---
+++ src/main/java/sn/zahra/thiaw/testspringcicdgithubaction/TestSpringCicdgIthubActionApplication.java
@@ -0,0 +1,13 @@
+package sn.zahra.thiaw.testspringcicdgithubaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringCicdgIthubActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringCicdgIthubActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sn/zahra/thiaw/testspringcicdgithubaction/TestSpringCicdgIthubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.zahra.thiaw.testspringcicdgithubaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringCicdgIthubActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
